48720328677157094e2f318290b454fd27c89b57
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-03-26 03:14:58 -0400

    Wrote constructor and destructor

---
+++ summit_climber_stepper_control/tic_stepper_controller.cpp
@@ -0,0 +1,17 @@
+#include "tic_stepper_controller.hpp"
+
+namespace ss
+{
+	TicDriver::TicDriver() : isInitialized(false)
+	{
+	}
+
+	TicDriver::~TicDriver()
+	{
+	}
+
+	int TicDriver::init(Serial *port, int initial_position)
+	{
+
+	}
+}
