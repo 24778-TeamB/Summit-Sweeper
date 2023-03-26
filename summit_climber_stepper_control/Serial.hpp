8aedf25e866bc667c5069e836524dfa5d8fb67af
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-03-25 22:28:54 -0400

    Created serial class

---
+++ summit_climber_stepper_control/Serial.hpp
@@ -0,0 +1,40 @@
+#ifndef __SERIAL_HPP__
+
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+#include <string>
+#include <memory>
+
+namespace sc
+{
+	typedef enum {
+		baud_9600,
+		baud_115200
+	} baud_rate_t;
+	class Serial
+	{
+	public:
+		Serial();
+		~Serial();
+		uint8_t Open(const char *port_name, baud_rate_t baud);
+		uint8_t Close();
+		uint8_t Write(uint8_t *buf, uint32_t numBytes);
+		uint8_t Read(uint8_t *buf, uint32_t numBytes);
+		uint8_t QueryBuffer();
+		uint8_t Flush();
+	private:
+		void Configure(baud_rate_t baud);
+
+		int port_fd;
+		std::string port_name;
+		bool isOpen;
+	};
+}
+
+#endif // !__SERIAL_HPP__
