cf889687269c7c18e689c82abe093e33bca99be3
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-03-29 01:29:13 -0400

    Wrote main routine

--- firmware/distance_sensors/distance_sensors.ino
+++ firmware/distance_sensors/distance_sensors.ino
@@ -1,9 +1,46 @@
-void setup()
+#include <stdint.h>
+#include "ProjectDef.h"
+#include "Commands.h"
+#include "CommandInterface.h"
+#include "ultrasonic.h"
+
+volatile uint32_t gEvents;
+uint16_t transfer_status;
+
+void setup(void)
 {
+  // Configure Serial
+  Serial.begin(BAUD);
+  CIClear();
   
+  // Configure sensors
+  init_ultrasonic();
+
+  gEvents = E_NO_EVENT;
 }
 
-void loop()
+void loop(void)
 {
-  
+  if (gEvents & E_SERIAL_ACTIVE)
+  {
+    transfer_status = GetCommand();
+    if (transfer_status == RX_COMPLETE)
+    {
+      DispatchCommand();
+    }
+    else if (transfer_status >= RX_OVERFLOW)
+    {
+      Serial.write("Receive Buffer Overflowed!");
+      CIClear();
+    }
+    gEvents &= ~E_SERIAL_ACTIVE;
+  }
+}
+
+void serialEvent(void)
+{
+  if (Serial.available())
+  {
+    gEvents |= E_SERIAL_ACTIVE;
+  }
 }
