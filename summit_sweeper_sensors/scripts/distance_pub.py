e24c8ded50f81ba30064a58cba2051351ea9559a
Tom Schmitz <tschmitz@andrew.cmu.edu>
2023-03-29 06:23:04 +0000

    Updated sensor publisher to write to arduino

--- summit_sweeper_sensors/scripts/distance_pub.py
+++ summit_sweeper_sensors/scripts/distance_pub.py
@@ -5,12 +5,23 @@ import std_msgs
 def main():
     port = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
     rospy.init_node('summit_sweeper_distance_sensors')
-    pub = rospy.Publisher('topic-name', std_msgs.msg.Float32MultiArray, queue_size=8)
-    randArray = [0.89, 0.95, 1.3, 1.5]
+    pub = rospy.Publisher('ultra-sonic', std_msgs.msg.Float32MultiArray, queue_size=8)
     pub_msg = std_msgs.msg.Float32MultiArray(data=randArray)
     pub.publish(pub_msg)
     while not rospy.is_shutdown():
-        pass
+        port.write('dist left\r\n'.encode('UTF-8'))
+        # Read data here
+        port.write('dist right\r\n'.encode('UTF-8'))
+        # Read and append data
+        port.write('dist forward\r\n'.encode('UTF-8'))
+        # Read and append data
+        port.write('dist down\r\n'.encode('UTF-8'))
+        # Read and append data
+        data = []  # TODO: convert data into list of floats
+        pub_msg = std_msgs.msg.Float32MultiArray(data=data)
+        pub.publish(pub_msg)
+        rospy.Rate(10).sleep()
+
 
 
 if __name__ == '__main__':
