21eaa0c42a292c75ed4536fe52c807b22809e3e4
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-03-29 01:26:47 -0400

    Wrote command

--- firmware/distance_sensors/Commands.cpp
+++ firmware/distance_sensors/Commands.cpp
@@ -23,7 +23,8 @@ int8_t CmdDist(char *cmdBuf, uint16_t bufSize, uint16_t argc, char const *const 
 {
 	int8_t retVal = RET_ERROR;
 	uint16_t token = OPTINVALID;
-	String floatBuffer;
+	ultrasonic_t data;
+	String floatBuffer[3];
 
 	if (argc < 2)
 	{
@@ -35,14 +36,27 @@ int8_t CmdDist(char *cmdBuf, uint16_t bufSize, uint16_t argc, char const *const 
 	switch (token)
 	{
 	case OPTLEFT:
+		data = getLeft();
 		break;
 	case OPTRIGHT:
+		data = getRight();
 		break;
 	case OPTFRONT:
+		data = getFront();
 		break;
 	case OPTDOWN:
+		data = getDown();
+		break;
+	default:
+		return retVal;
 		break;
 	}
+
+	floatBuffer[0] = String(data.headModule, 2);
+	floatBuffer[1] = String(data.middleModule, 2);
+	floatBuffer[2] = String(data.tailModule, 2);
+	snprintf(cmdBuf, bufSize, "%s, %s, %s", floatBuffer[0].c_str(), floatBuffer[1].c_str(), floatBuffer[2].c_str());
+	retVal = RET_QUIET;
 
 	return retVal;
 }
