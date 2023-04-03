4dfbbfe2e9cfd37e91755474eea8aebdb529864d
Tom Schmitz <tschmitz@andrew.cmu.edu>
2023-04-03 01:38:41 +0000

    Now blocking keyboard interrupt during cleanup

--- summit_sweeper_stepper_control/scripts/vertical_control.py
+++ summit_sweeper_stepper_control/scripts/vertical_control.py
@@ -3,12 +3,31 @@ import std_msgs
 import threading
 from ticlib import TicUSB, TIC36v4
 from time import sleep
+import signal
+import logging
 
 mtx1 = threading.Lock()
 mtx2 = threading.Lock()
 
 frontTic = TicUSB(product=TIC36v4, serial_number='00414637')
 # rearTic = TicUSB(product=TIC36v4, serial_number=None)
+
+
+# https://stackoverflow.com/a/21919644/11854714
+class DelayedKeyboardInterrupt:
+
+    def __enter__(self):
+        self.signal_received = False
+        self.old_handler = signal.signal(signal.SIGINT, self.handler)
+
+    def handler(self, sig, frame):
+        self.signal_received = (sig, frame)
+        logging.debug('SIGINT received. Delaying KeyboardInterrupt.')
+
+    def __exit__(self, type, value, traceback):
+        signal.signal(signal.SIGINT, self.old_handler)
+        if self.signal_received:
+            self.old_handler(*self.signal_received)
 
 
 def callbackSetPos1(pos: std_msgs.msg.Int32):
@@ -76,22 +95,27 @@ def main():
             print(str(e))
 
     # Cleanup
-    mtx1.acquire()  # Lock mutex just in case
-    print('Resetting stepper motors')
-    frontTic.set_target_position(0)
-    while frontTic.get_current_position() != frontTic.get_target_position():
-        sleep(0.1)
+    with DelayedKeyboardInterrupt():
+        mtx1.acquire()  # Lock mutex just in case
+        print('Resetting stepper motors')
+        frontTic.set_target_position(0)
+        while frontTic.get_current_position() != frontTic.get_target_position():
+            sleep(0.1)
 
-    frontTic.deenergize()
-    frontTic.enter_safe_start()
-    mtx1.release()
+        frontTic.deenergize()
+        frontTic.enter_safe_start()
+        mtx1.release()
 
-    mtx2.acquire()
-    # rearTic.set_target_position(0)
-    # while rearTic.get_current_position() != rearTic.get_target_position():
-    #     sleep(0.1)
+        mtx2.acquire()
+        # rearTic.set_target_position(0)
+        # while rearTic.get_current_position() != rearTic.get_target_position():
+        #     sleep(0.1)
 
-    # rearTic.deenergize()
-    # rearTic.enter_safe_start()
-    mtx2.release()
+        # rearTic.deenergize()
+        # rearTic.enter_safe_start()
+        mtx2.release()
+
+
+if __name__ == '__main__':
+    main()
 
