d86d583d7b33367e7ad5cee7d8277ebe2f53b93d
Tom Schmitz <tschmitz@andrew.cmu.edu>
2023-04-07 04:41:09 +0000

    Fixed buggy code

--- summit_sweeper_sensors/scripts/distance_pub.py
+++ summit_sweeper_sensors/scripts/distance_pub.py
@@ -3,29 +3,24 @@ import serial
 import std_msgs
 
 def main():
-    port = serial.Serial('/dev/ttyACM0', 9600, timeout=None)
+    port = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
     rospy.init_node('summit_sweeper_distance_sensors')
     pub = rospy.Publisher('ultra_sonic', std_msgs.msg.Float32MultiArray, queue_size=8)
     waitBuf = rospy.Rate(1000)
     while not rospy.is_shutdown():
-        port.write('dist left\r\n'.encode('UTF-8'))
-        leftData = port.read_until(b'>')
+        port.write(b'dist left\r\n')
+        leftData = port.readline()
         waitBuf.sleep()
-        port.reset_input_buffer()
-        port.write('dist right\r\n'.encode('UTF-8'))
-        rightData = port.read_until(b'>')
+        port.write(b'dist right\r\n')
+        rightData = port.readline()
         waitBuf.sleep()
-        port.reset_input_buffer()
-        port.write('dist forward\r\n'.encode('UTF-8'))
-        port.read_until(b'>')
+        port.write(b'dist forward\r\n')
+        port.readline()
         waitBuf.sleep()
-        forwardData = port.reset_input_buffer()
-        port.write('dist down\r\n'.encode('UTF-8'))
-        downData = port.read_until(b'>')
+        port.write(b'dist down\r\n')
+        downData = port.readline()
         waitBuf.sleep()
-        port.reset_input_buffer()
-        data = leftData.decode('UTF-8').split(', ') + rightData.decode('UTF-8').split(', ') + forwardData.decode(
-            'UTF-8').split(', ') + downData.decode('UTF-8').split(', ')
+        data = leftData.decode('UTF-8')[:-2].split(', ') + rightData.decode('UTF-8')[:-2].split(', ') + forwardData.decode('UTF-8')[:-2].split(', ') + downData.decode('UTF-8')[:-2].split(', ')
         try:
             data = [float(d) for d in data]
             pub_msg = std_msgs.msg.Float32MultiArray(data=data)
