bec55a823f4a176c5963b4397e23046d7d26d98a
Tom Schmitz <tschmitz@andrew.cmu.edu>
2023-04-09 18:52:40 +0000

    Started on step state machine

--- summit_sweeper_run/scripts/summit_sweeper.py
+++ summit_sweeper_run/scripts/summit_sweeper.py
@@ -6,7 +6,11 @@ import enum
 
 
 mtx = threading.Lock()
+s_mtx1 = threading.Lock()
+s_mtx2 = threading.Lock()
 SENSOR_READINGS = []
+step1_pos = 0
+step2_pos = 0
 
 DC_MOTOR = {
         'forward': 3,
@@ -40,6 +44,11 @@ class currentState(enum.Enum):
     FINISHED = 0x3
 
 
+class stepStateMachine:
+    def __init__(self, front, rear):
+        self.frontPos = front
+
+
 def wait_for_subscribers(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub):
     i = 0
     while not rospy.is_shutdown() and (horizontal_pub.get_num_connections() == 0 or vertical_pub1.get_num_connections() == 0 or vertical_pub2.get_num_connections() == 0 or vacuum_pub.get_num_connections() == 0):
@@ -65,8 +74,25 @@ def sensor_callback(data: Float32MultiArray):
     mtx.release()
     return
 
+def stepper1_position(data: Int32):
+    global s_mtx1
+    global step1_pos
+    s_mtx1.acquire()
+    step1_pos = data.data
+    s_mtx1.release()
+    return
 
-def clean_down(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub, readings):
+
+def stepper2_position(data: Int32):
+    global s_mtx2
+    global step2_pos
+    s_mtx2.acquire()
+    step2_pos = data.data
+    s_mtx2.release()
+    return
+
+
+def clean_down(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub, readings, front, rear):
     global current_state
     if current_state == CLEAN_STATE['left']:
         pass
@@ -78,7 +104,7 @@ def clean_down(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub, reading
         pass
 
 
-def clean_up(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub, readings):
+def clean_up(horizontal_pub, vertical_pub1, vertical_pub2, vacuum_pub, readings, front, rear):
     global current_state
     if current_state == CLEAN_STATE['left']:
         pass
@@ -95,6 +121,7 @@ def main():
     global SENSOR_READINGS
     rospy.init_node('summit_sweeper_main_run')
     rospy.Subscriber('ultra_sonic', Float32MultiArray, sensor_callback)
+    rospy.Subscriber('front_stepper'
     vacuum_pub = rospy.Publisher('vacuum_control_sub', Int8, queue_size = 1)
     horizontal_pub = rospy.Publisher('horizontal_control', Int8, queue_size = 8)
     vert_movement1 = rospy.Publisher('front_vert_control', Int32, queue_size = 8)
@@ -107,16 +134,22 @@ def main():
         mtx.acquire()
         readings = copy.deepcopy(SENSOR_READINGS)
         mtx.release()
+        s_mtx1.acquire()
+        front = step1_pos
+        s_mtx1.release()
+        s_mtx2.acquire()
+        rear = step2_pos
+        s_mtx2.release()
 
         if state == currentState.CLEAN_UP:
-            clean_up(horizontal_pub, vert_movement1, vert_movement2, vacuum_pub, readings)
+            clean_up(horizontal_pub, vert_movement1, vert_movement2, vacuum_pub, readings, front, rear)
             # check if job is finished here
         elif state == currentState.CLEAN_DOWN:
-            clean_down(horizontal_pub, vert_movement1, vert_movement2, vacuum_pub, readings)
+            clean_down(horizontal_pub, vert_movement1, vert_movement2, vacuum_pub, readings, front, rear)
             # Check if job is finished here
         elif state == currentState.FINISHED:
             vacuum_pub.publish(Int8(data=VACUUM['off']))
-            vert_movement1.publish(Int32(data=0))
+            vert_movement1.publish(Int32(data=0)) # Initialize one of these to -15170
             vert_movement2.publish(Int32(data=0))
             horizontal_pub.publish(Int8(data=DC_MOTOR['forward']))
             rospy.Rate(1).sleep()
