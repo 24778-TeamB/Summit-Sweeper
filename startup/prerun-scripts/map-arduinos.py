bac63a3f84c93fabc613588ac029c5d4c680677b
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-04-13 01:07:14 -0400

    Wrote script to map arduino ports

---
+++ startup/prerun-scripts/map-arduinos.py
@@ -0,0 +1,34 @@
+import sys
+from serial.tools.list_ports import comports
+
+MAPPINGS = {
+    'sensors': '7513131393235120F102',
+    'motors': ''
+}
+
+def parse_serial_number(hwid: str) -> str:
+    return hwid.split(' ')[2].split('=')[1]
+
+def find_port(FTDI_SN: str) -> str:
+    ports = comports()
+    for port in ports:
+        sn = parse_serial_number(port.hwid)
+        if sn == FTDI_SN:
+            return port.name
+    raise Exception(f'Port not found for {FTDI_SN}')
+
+def print_usage() -> None:
+    print('USAGE: python3 map-arduinos.py [sensors | motors]')
+    sys.exit()
+
+def main(mapping):
+    if mapping not in MAPPINGS.keys():
+        print_usage()
+    port = find_port(MAPPINGS[mapping])
+    print(port)
+    return
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print_usage()
+    main(sys.argv[1])
