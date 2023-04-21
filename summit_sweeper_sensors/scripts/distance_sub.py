48f38cda3d93d52f4a2e1f5202a47a52fca08e3b
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-04-21 16:42:12 -0400

    Updated distance sub

--- summit_sweeper_sensors/scripts/distance_sub.py
+++ summit_sweeper_sensors/scripts/distance_sub.py
@@ -2,7 +2,7 @@ import rospy
 import std_msgs
 
 
-def callback_listener(dataValues: std_msgs.msg.Float32MultiArray):
+def callback_listener(dataValues: std_msgs.msg.UInt8MultiArray):
     rospy.loginfo(f'{dataValues.data}')
     return
 
@@ -10,7 +10,7 @@ def main():
     global kf
     global mutex
     rospy.init_node('summit_sweeper_distance_sub')
-    rospy.Subscriber('ultra_sonic', std_msgs.msg.Float32MultiArray, callback_listener)
+    rospy.Subscriber('ir_sensor', std_msgs.msg.UInt8MultiArray, callback_listener)
     rospy.spin()
 
 if __name__ == '__main__':
