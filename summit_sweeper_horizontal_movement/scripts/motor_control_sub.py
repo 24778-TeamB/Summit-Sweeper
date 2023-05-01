1e1bd68747911ecc7194a87706f12bb6bde33498
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-05-01 15:39:32 -0400

    Optimized code

--- summit_sweeper_horizontal_movement/scripts/motor_control_sub.py
+++ summit_sweeper_horizontal_movement/scripts/motor_control_sub.py
@@ -94,21 +94,15 @@ def issueMovement(msg: Int8):
         if port is None:
             rospy.logerr('Unable to write to motor controller: port is closed!')
             return
+        port.write(f'set speed r1 {MID_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
+        port.write(f'set speed r2 {REAR_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
+        port.write(f'set speed r3 {FRONT_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
+        port.write(f'set speed l1 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
+        port.write(f'set speed l2 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
+        port.write(f'set speed l3 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
         if MOVEMENT[movement].startswith('motor'):
-            port.write(f'set speed r1 {MID_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed r2 {REAR_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed r3 {FRONT_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l1 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l2 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l3 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
             port.write(f'{MOVEMENT[movement]}\r\n'.encode('UTF-8'))
         else:
-            port.write(f'set speed r1 {MID_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed r2 {REAR_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed r3 {FRONT_RIGHT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l1 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l2 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
-            port.write(f'set speed l3 {MID_LEFT_SPEEDS[MOVEMENT[movement]]}\r\n'.encode('UTF-8'))
             port.write(b'motor forward\r\n')
     except serial.SerialTimeoutException:
         rospy.logerr('Horizontal Motor Controller Timed out!')
