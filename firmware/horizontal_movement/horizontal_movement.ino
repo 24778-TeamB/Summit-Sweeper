f1cb4b27ef6509487599cdd1928c2281efc48845
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-05-03 16:35:50 -0400

    Fixed bug

--- firmware/horizontal_movement/horizontal_movement.ino
+++ firmware/horizontal_movement/horizontal_movement.ino
@@ -18,12 +18,15 @@ void motorsCallback(const std_msgs::UInt8MultiArray & motorSpeeds)
 
 	direction = motorSpeeds.data[0];
 
-	for (uint16_t i = 1; i < NUM_MOTORS; i++)
+	for (uint16_t i = 1; i < (NUM_MOTORS + 1); i++)
 	{
+    nh.loginfo(String(motorSpeeds.data[i]).c_str());
 		speed[i - 1] = motorSpeeds.data[i];
 	}
 
 	updateMotors(direction, speed);
+
+ nh.logdebug("Finished speed updates");
 }
 
 ros::Subscriber<std_msgs::UInt8MultiArray> sub("horizontal_control", &motorsCallback);
