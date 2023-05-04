c740995d9412c8383d6fc77c91bc38fb5fbc00e1
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-05-04 18:00:27 -0400

    Updated integration

--- summit_sweeper_run/scripts/summit_sweeper.py
+++ summit_sweeper_run/scripts/summit_sweeper.py
@@ -3,20 +3,14 @@ from std_msgs.msg import UInt8MultiArray, Int8, Int32
 import threading
 import copy
 import enum
+from typing import Dict, Tuple
+import requests
 
-
-DC_MOTOR = {
-        'forward': Int8(data=3),
-        'reverse': Int8(data=4),
-        'left': Int8(data=1),
-        'right': Int8(data=2),
-        'stop': Int8(data=0)
-        }
 
 VACUUM = {
-        'on': 1,
-        'off': 0,
-        'test': 2
+        'on': Int8(data=1),
+        'off': Int8(data=0),
+        'test': Int8(data=2)
         }
 
 SENSOR_INDEX = {
@@ -29,6 +23,179 @@ SENSOR_INDEX = {
         }
 
 
+class horizontalSpeeds(dict):
+    def __init__(self, url: str):
+        super().__init__()
+        frontRight, frontLeft, midRight, midLeft, rearRight, rearLeft = self._load_speed_configs(url)
+        _preparedMessages = {
+            'forward': UInt8MultiArray(data=[3, frontRight['forward'], frontLeft['forward'], midRight['forward'],
+                                             midLeft['forward'], rearRight['forward'], rearLeft['forward']]),
+            'reverse': UInt8MultiArray(data=[4, frontRight['reverse'], frontLeft['reverse'], midRight['reverse'],
+                                             midLeft['reverse'], rearRight['reverse'], rearLeft['reverse']]),
+            'left': UInt8MultiArray(data=[2, frontRight['left'], frontLeft['left'], midRight['left'],
+                                          midLeft['left'], rearRight['left'], rearLeft['left']]),
+            'right': UInt8MultiArray(data=[1, frontRight['right'], frontLeft['right'], midRight['right'],
+                                           midLeft['right'], rearRight['right'], rearLeft['right']]),
+            'cw': UInt8MultiArray(data=[3, frontRight['cw'], frontLeft['cw'], midRight['cw'],
+                                        midLeft['cw'], rearRight['cw'], rearLeft['cw']]),
+            'ccw': UInt8MultiArray(data=[3, frontRight['ccw'], frontLeft['ccw'], midRight['ccw'],
+                                         midLeft['ccw'], rearRight['ccw'], rearLeft['ccw']]),
+            'climb': UInt8MultiArray(data=[3, frontRight['climb'], frontLeft['climb'], midRight['climb'],
+                                           midLeft['climb'], rearRight['climb'], rearLeft['climb']]),
+            'stop': UInt8MultiArray(data=[0, frontRight['stop'], frontLeft['stop'], midRight['stop'],
+                                          midLeft['stop'], rearRight['stop'], rearLeft['stop']]),
+        }
+        self.__dict__ = _preparedMessages
+
+    @staticmethod
+    def _load_speed_configs(url) -> Tuple[Dict[str, int], Dict[str, int], Dict[str, int], Dict[str, int], Dict[str, int], Dict[str, int]]:
+        frontRight = {}
+        frontLeft = {}
+        midRight = {}
+        midLeft = {}
+        rearRight = {}
+        rearLeft = {}
+        f = requests.get(url)
+        data = f.json()
+        data = data['motor-speeds']
+        for motor in data:
+            if motor['motor-set'] == 'front' and motor['side'] == 'right':
+                for speeds in motor['speeds']:
+                    frontRight[speeds['movement']] = speeds['speed']
+            if motor['motor-set'] == 'front' and motor['side'] == 'left':
+                for speeds in motor['speeds']:
+                    frontLeft[speeds['movement']] = speeds['speed']
+            if motor['motor-set'] == 'middle' and motor['side'] == 'right':
+                for speeds in motor['speeds']:
+                    midRight[speeds['movement']] = speeds['speed']
+            if motor['motor-set'] == 'middle' and motor['side'] == 'left':
+                for speeds in motor['speeds']:
+                    midLeft[speeds['movement']] = speeds['speed']
+            if motor['motor-set'] == 'rear' and motor['side'] == 'right':
+                for speeds in motor['speeds']:
+                    rearRight[speeds['movement']] = speeds['speed']
+            if motor['motor-set'] == 'rear' and motor['side'] == 'left':
+                for speeds in motor['speeds']:
+                    rearLeft[speeds['movement']] = speeds['speed']
+        return frontRight, frontLeft, midRight, midLeft, rearRight, rearLeft
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+
+# TODO Update this to do one trip
+class HorizontalMovement:
+    class Direction(enum.Enum):
+        STOP = 0x0
+        FORWARD = 0x1
+        LEFT = 0x2
+        RIGHT = 0x3
+        CW = 0x4
+        CCW = 0x5
+
+    def __init__(self, dcMotorPub, startingPosLeft: bool, speed_profile: horizontalSpeeds):
+        self.direction = self.Direction.STOP
+        self._completedStates = {'left': False, 'right': False}
+        self.dc_motors = dcMotorPub
+        self.lastMovement = self.Direction.STOP
+        self._newStep = True
+        if startingPosLeft:
+            self._startDirection = self.Direction.RIGHT
+        else:
+            self._startDirection = self.Direction.LEFT
+        self._cleanDirection = self._startDirection
+        self.dc_movements = speed_profile
+
+    def _correctOrientation(self, readings):
+        # Both sensors are off the wall, move forward
+        if readings[SENSOR_INDEX['center-left']] and readings[SENSOR_INDEX['center-right']]:
+            self.direction = self.Direction.FORWARD
+            if self.direction != self.lastMovement:
+                self.lastMovement = self.Direction.FORWARD
+                rospy.loginfo('Forward')
+                self.dc_motors.publish(self.dc_movements['forward'])
+        # Right sensor off the wall, rotate CCW
+        elif readings[SENSOR_INDEX['center-right']] and not readings[SENSOR_INDEX['center-left']]:
+            self.direction = self.Direction.CW
+            if self.direction != self.lastMovement:
+                self.lastMovement = self.Direction.CW
+                rospy.loginfo('cw')
+                self.dc_motors.publish(self.dc_movements['cw'])
+        # Left sensor off the wall, rotate CW
+        elif not readings[SENSOR_INDEX['center-right']] and readings[SENSOR_INDEX['center-left']]:
+            self.direction = self.Direction.CCW
+            if self.direction != self.lastMovement:
+                self.lastMovement = self.Direction.CCW
+                self.dc_motors.publish(self.dc_movements['ccw'])
+                rospy.loginfo('ccw')
+        return
+
+    def _cleanRight(self, readings) -> bool:
+        if readings[SENSOR_INDEX['center-right']] or readings[SENSOR_INDEX['center-left']]:
+            self._correctOrientation(readings)
+            return False
+        elif not readings[SENSOR_INDEX['side-right']]:
+            self.direction = self.Direction.STOP
+            if self.direction != self.lastMovement:
+                self.lastMovement = self.Direction.STOP
+                self.dc_motors.publish(self.dc_movements['stop'])
+                rospy.loginfo('stop')
+            self._completedStates['right'] = True
+            return True
+        self.direction = self.Direction.RIGHT
+        if self.direction != self.lastMovement:
+            self.lastMovement = self.Direction.RIGHT
+            self.dc_motors.publish(self.dc_movements['right'])
+            rospy.loginfo('right')
+        return False
+
+    def _cleanLeft(self, readings) -> bool:
+        if readings[SENSOR_INDEX['center-right']] or readings[SENSOR_INDEX['center-left']]:
+            self._correctOrientation(readings)
+            return False
+        elif not readings[SENSOR_INDEX['side-left']]:
+            self.direction = self.Direction.STOP
+            if self.direction != self.lastMovement:
+                self.lastMovement = self.Direction.STOP
+                self.dc_motors.publish(self.dc_movements['stop'])
+                rospy.loginfo('stop')
+            self._completedStates['left'] = True
+            return True
+        self.direction = self.Direction.LEFT
+        if self.direction != self.lastMovement:
+            self.lastMovement = self.Direction.LEFT
+            self.dc_motors.publish(self.dc_movements['left'])
+            rospy.loginfo('left')
+        return False
+
+    def resetStateMachine(self) -> None:
+        self.dc_motors.publish(self.dc_movements['stop'])
+        self.direction = self.Direction.STOP
+        self.lastMovement = self.Direction.STOP
+        self._completedStates['left'] = False
+        self._completedStates['right'] = False
+        self._newStep = True
+        rospy.loginfo('Reset state machine')
+        return
+
+    def next(self, readings) -> bool:
+        if self._newStep:
+            self._newStep = False
+            self.direction = self._startDirection
+        if self._cleanDirection == self.Direction.LEFT:
+            if self._cleanLeft(readings):
+                self._cleanDirection = self.Direction.RIGHT
+        elif self._cleanDirection == self.Direction.RIGHT:
+            if self._cleanRight(readings):
+                self._cleanDirection = self.Direction.LEFT
+        else:
+            rospy.logerr(f'Invalid horizontal movement state: {self._cleanDirection}')
+        return self._completedStates['right'] and self._completedStates['left']
+
+
 class stepStateMachine:
     class climbState(enum.Enum):
         CLEAN = 0X0
@@ -37,18 +204,15 @@ class stepStateMachine:
         LIFT_ENDS = 0x3
         FORWARD2 = 0X4
 
-    def __init__(self, dc_motor_pub, vacuum, frontL = 0, rearL = 0, frontH = 1, rearH = 1):
-        self.frontTargets = {}
-        self.rearTargets = {}
-        self.frontTargets['low'] = Int32(data=frontL)
-        self.frontTargets['high'] = Int32(data=frontH)
-        self.rearTargets['low'] = Int32(data=rearL)
-        self.rearTargets['high'] = Int32(data=rearH)
+    def __init__(self, dc_motor_pub, speed_profile: horizontalSpeeds, vacuum, frontL=0, rearL=0, frontH=1, rearH=1):
+        self.frontTargets = {'low': Int32(data=frontL), 'high': Int32(data=frontH)}
+        self.rearTargets = {'low': Int32(data=rearL), 'high': Int32(data=rearH)}
         self.mtx1 = threading.Lock()
         self.mtx2 = threading.Lock()
         self.frontPos = 0
         self.rearPos = 0
         self.currentState = self.climbState.CLEAN
+        self.dc_movement = speed_profile
 
         self.vert_movement1 = rospy.Publisher('front_vert_control', Int32, queue_size=8)
         self.vert_movement2 = rospy.Publisher('rear_vert_control', Int32, queue_size=8)
@@ -84,25 +248,23 @@ class stepStateMachine:
             elif self.currentState == self.climbState.LIFT_MIDDLE:
                 if self.frontPos == self.frontTargets['low'].data and self.rearPos == self.rearTargets['low'].data:
                     self.currentState = self.climbState.FORWARD1
+                    self.dc_pub.publish(self.dc_movement['climb'])
             elif self.currentState == self.climbState.FORWARD1:
                 if not readings[SENSOR_INDEX['rear-right']] and not readings[SENSOR_INDEX['rear-left']]:
-                    self.dc_pub.publish(DC_MOTOR['stop'])
+                    self.dc_pub.publish(self.dc_movement['stop'])
                     self.currentState = self.climbState.LIFT_ENDS
                     self.vert_movement1.publish(self.frontTargets['high'])
                     self.vert_movement2.publish(self.rearTargets['high'])
-                else:
-                    self.dc_pub.publish(DC_MOTOR['forward'])
             elif self.currentState == self.climbState.LIFT_ENDS:
                 if self.frontPos == self.frontTargets['high'].data and self.rearPos == self.rearTargets['high'].data:
                     self.currentState = self.climbState.FORWARD2
+                    self.dc_pub.publish(self.dc_movement['climb'])
             elif self.currentState == self.climbState.FORWARD2:
                 if not readings[SENSOR_INDEX['center-right']] and not readings[SENSOR_INDEX['center-left']]:
-                    self.dc_pub.publish(DC_MOTOR['stop'])
+                    self.dc_pub.publish(self.dc_movement['stop'])
                     self.currentState = self.climbState.CLEAN
-                    self.vacuum.publish(Int8(data=VACUUM['on']))
+                    self.vacuum.publish(VACUUM['on'])
                     finished = True
-                else:
-                    self.dc_pub.publish(DC_MOTOR['forward'])
         else:
             rospy.logerr('Not implemented')
         self.mtx1.release()
@@ -113,15 +275,13 @@ class stepStateMachine:
 class cleanStateMachine:
     class currentState(enum.Enum):
         INITIALIZATION = 0x0
-        CLEAN_LEFT = 0x1
-        CLEAN_RIGHT = 0x2
-        STEP = 0x3
-        FINISHED = 0x4
+        CLEAN = 0x1
+        STEP = 0x2
+        FINISHED = 0x3
 
-    def __init__(self, up: bool = True):
+    def __init__(self, speed_profile: horizontalSpeeds, up: bool = True, startingLeft: bool = True):
         self.up = up
         self.current_state = self.currentState.STEP
-        self.lastRun = self.currentState.CLEAN_LEFT
 
         self.sensor_mtx = threading.Lock()
         self.readings = []
@@ -130,8 +290,9 @@ class cleanStateMachine:
         self.vacuum_pub = rospy.Publisher('vacuum_control_sub', Int8, queue_size=1)
         self.horizontal_movement = rospy.Publisher('horizontal_control', Int8, queue_size=4)
 
-        self.step = stepStateMachine(self.horizontal_movement, self.vacuum_pub, frontL = -16700, rearL = -16700,
-                                     frontH=0, rearH=0)
+        self.step = stepStateMachine(self.horizontal_movement, speed_profile, self.vacuum_pub, frontL = -16920,
+                                     rearL = -16920, frontH=0, rearH=0)
+        self.horizontal = HorizontalMovement(self.horizontal_movement, startingLeft, speed_profile)
 
         self._wait_for_subscribers()
 
@@ -162,68 +323,31 @@ class cleanStateMachine:
         self.sensor_mtx.release()
         return
 
-    def _moveRight(self, readings) -> bool:
-        # TODO: Figure out better way to hug riser
-        finished = False
-        if readings[SENSOR_INDEX['center-right']] or readings[SENSOR_INDEX['center-left']]:
-            self.horizontal_movement.publish(DC_MOTOR['forward'])
-        elif not readings[SENSOR_INDEX['side-right']]:
-            self.horizontal_movement.publish(DC_MOTOR['stop'])
-            finished = True
-        else:
-            self.horizontal_movement.publish(DC_MOTOR['right'])
-        return finished
-
-    def _moveLeft(self, readings) -> bool:
-        # TODO: Figure out better way to hug riser
-        finished = False
-        if readings[SENSOR_INDEX['center-right']] or readings[SENSOR_INDEX['center-left']]:
-            self.horizontal_movement.publish(DC_MOTOR['forward'])
-        elif not readings[SENSOR_INDEX['side-left']]:
-            self.horizontal_movement.publish(DC_MOTOR['stop'])
-            finished = True
-        else:
-            self.horizontal_movement.publish(DC_MOTOR['left'])
-        return finished
-
     def next(self):
-        refreshRate = 2
+        refreshRate = 1
         self.sensor_mtx.acquire()
         readings = copy.deepcopy(self.readings)
         self.sensor_mtx.release()
 
         if self.current_state == self.currentState.INITIALIZATION:
-            result = self._moveLeft(readings)
-            if result:
-                self.vacuum_pub.publish(Int8(data=VACUUM['on']))
-                self.current_state = self.currentState.CLEAN_RIGHT
-                self.lastRun = self.currentState.CLEAN_RIGHT
-        elif self.current_state == self.currentState.CLEAN_LEFT:
-            finished = self._moveLeft(readings)
-            if finished:
-                self.lastRun = self.currentState.CLEAN_RIGHT
-                self.current_state = self.currentState.STEP
-        elif self.current_state == self.currentState.CLEAN_RIGHT:
-            finished = self._moveRight(readings)
-            if finished:
-                self.lastRun = self.currentState.CLEAN_RIGHT
+            self.vacuum_pub.publish(VACUUM['on'])
+            self.current_state = self.currentState.CLEAN
+        elif self.current_state == self.currentState.CLEAN:
+            if self.horizontal.next(readings):
                 self.current_state = self.currentState.STEP
         elif self.current_state == self.currentState.STEP:
-            done = self.step.next(readings, self.up)
-            if done:
-                if self.lastRun == self.currentState.CLEAN_LEFT:
-                    self.current_state = self.currentState.CLEAN_RIGHT
-                else:
-                    self.current_state = self.currentState.CLEAN_LEFT
-            else:
-                refreshRate = 10
-                # TODO: Figure out when finished
+            if self.step.next(readings, self.up):
+                # TODO: Figure out when to stop
+                self.current_state = self.currentState.CLEAN
+            refreshRate = 10
+
         return refreshRate, self.current_state == self.currentState.FINISHED
 
 
 def main():
     rospy.init_node('summit_sweeper_main_run')
-    clean = cleanStateMachine()
+    dc_speeds = horizontalSpeeds('https://raw.githubusercontent.com/24778-TeamB/motor-speeds/master/speeds.json')
+    clean = cleanStateMachine(dc_speeds)
 
     finished = False
 
